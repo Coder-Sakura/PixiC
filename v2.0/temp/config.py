a59a53130674a663bf6895356f4eeb8b4bee68b1
Coder-Sakura <1508015265@qq.com>
2026-01-19 15:13:41 +0800

    改进config配置文件的注释和结构

--- v2.0/temp/config.py
+++ v2.0/temp/config.py
@@ -1,112 +1,76 @@
 # coding=utf8
-# ================================================
-# 关注画师路径为空时,在脚本当前路径创建pixiv_crawler文件夹
-# 示例,D:\pixiv_crawler
-ROOT_PATH = r''
 
-# 收藏作品路径为空时,在脚本当前路径创建bookmark文件夹
-# 示例,D:\bookmark
-BOOKMARK_PATH = r''
+# ================== FOLDER ==================
+ROOT_PATH = r''							# 关注画师作品存放路径, 路径为空时在脚本目录创建
+BOOKMARK_PATH = r''						# 收藏作品存放路径, 路径为空时在脚本目录创建
 
-# Chrome浏览器用户数据目录
-# 根据wiki进行更改,此为路径参考,一般只需要将<user>更改为你的用户名
+# Chrome浏览器用户数据目录, 通常只需将 Users 更改为你的用户名
 # PRO_DIR = r'C:\Users\Administrator\AppData\Local\Google\Chrome\User Data'
 PRO_DIR = r''
-# ================================================
+# ================== FOLDER ==================
 
 
-# ================账户uid及cookie================
-# uid用于获取对应用户的关注、收藏作品
-# 一般是下载自己的pixiv关注和收藏,如需下载他人的
-# 请填写他人的uid并选择另外的目录作为下载目录,避免存放到之前的目录中
-USER_ID = ""
-
+# ================== LOGIN ==================
+USER_ID = ""							# Pixiv 账户的 uid
 
 """
-1. 设置该项以配置是否启动chrome浏览器更新本地cookie
-2. 首次运行请设置为True,后续运行可设置为False
-3. 该项设置为False及本地无cookie文件,会引发[Errno 2]异常
-4. 若不想配置chrome可将此项设置为False,但要进行ORIGI_COOKIE_LIST的配置
+1. 启用以持久化 Chrome 已登录的 Pixiv 账号信息
+2. 建议: 首次运行启用, 后续运行设置为 False
+3. 若不想配置 Chrome, 配置此项为 False 并配置 ORIGI_COOKIE_LIST
 """
 COOKIE_UPDATE_ENABLED = True
 # COOKIE_UPDATE_ENABLED = False
 
-
 """
-1. 自定义cookie,将user_cookie替换为你复制的pixiv cookie
-2. 若有多个,则依次复制替换,注意结尾的','
-ORIGI_COOKIE_LIST = [
-	'user_cookie1',     
-	'user_cookie2',
-]
-
-[WARNING]
-1. 注意! 当自定义多个cookie时,请填写'USER_ID'字段
-2. 否则将从指定的多个cookie中随机选取一个以获得对应的uid
-3. 但这并不一定是你想要的uid,下载下来的关注作品和收藏作品
-也不一定是你想要的
+1. 配置 cookie, 一行一个完整cookie
+2. 多个 cookie 时请配置 USER_ID, 否则从中随机一个进行获取
 """
 ORIGI_COOKIE_LIST = []
-# ================================================
+# ================== LOGIN ==================
 
 
-# ======控制开关========
-# 关注画师爬虫控制开关
-PIXIV_CRAWLER_ENABLED = True
+# ================== LOOP & LIMIT ==================
+SKIP_EXISTS_ILLUST = True							# 增量&快速更新 默认开启
+													# 启用时不更新数据库数据,减少请求次数
+SLOW_MODE = True									# 慢速模式抓取 线程数固定为1 默认开启 不建议改动
+													# 配置为 False 以开启快速模式
 
-# 收藏作品爬虫控制开关
-PIXIV_BOOKMARK_ENABLED = True
+PIXIV_CRAWLER_ENABLED = False						# 关注画师模块开关 默认关闭
+PIXIV_BOOKMARK_ENABLED = True						# 收藏作品模块开关 默认开启
+BOOKMARK_HIDE_ENABLE = False						# 除非有未公开收藏否则关闭
+THREAD_NUM = 4										# 为关注画师/收藏作品模块各分配的线程数 不建议改动
+LOOP_LIMIT = 0										# 单轮次各模块抓取上限 默认0 0为不限制
+													# 建议增量&快速更新设置为8000,避免过多请求导致账号被封
 
-# API控制开关
-PIXIV_API_ENABLED = False
-# ================================================
+USERS_CYCLE = 86400									# 关注画师 检测周期 默认86400秒
+USERS_LIMIT = 3000									# 关注画师作品 最低下载收藏数 默认3000
+													# 仅收集作品元数据 可设置为99999
 
-
-# ===============检测周期&最低限制=================
-# 关注-画师作品检测周期,单位秒,默认86400秒
-USERS_CYCLE = 86400
-
-# 关注-最低收藏限制,高于LIMIT才会下载,默认3000
-# 若只想要数据库记录,可设置为99999
-USERS_LIMIT = 3000
-
-# 收藏-作品检测周期,单位秒,默认86400秒
-BOOKMARK_CYCLE = 86400
-
-# 收藏-最低收藏限制,高于LIMIT才会下载,默认为0
-# 若只想要数据库记录,可设置为99999
-BOOKMARK_LIMIT = 0
-# ================================================
+BOOKMARK_CYCLE = 86400								# 收藏作品 检测周期 默认86400秒
+BOOKMARK_LIMIT = 0									# 收藏作品作品 最低下载收藏数 默认0
+													# 仅收集作品元数据 可设置为99999
+# ================== LOOP & LIMIT ==================
 
 
-# ==================API设置=======================
+# ================== API ==================
+PIXIV_API_ENABLED = False					# API 控制开关 默认关闭
 API_HOST = '0.0.0.0'
-# 2020/5/10 pixiv网站排名 + 5
 API_PORT = '1526'
-# API数据库线程数
-API_THREAD = 8
-# API-random接口-最大返回数
-RANDOM_LIMIT = 10
-# ================================================
+API_THREAD = 8								# API 数据库线程数
+RANDOM_LIMIT = 10							# random 接口最大返回数
+# ================== API ==================
 
 
-# ===============数据库连接信息====================
-# 数据库开关,默认为关
-# True会存储数据到数据库
-# False则不会存储数据,但同时部分API也无法使用
-# DB_ENABLE = True
-DB_ENABLE = False
-
-# create.sql在doc目录下
+# ================== DataBase ==================
+DB_ENABLE = False								# 数据库开关 默认关 不启用时API不生效
 DB_HOST = 'localhost'
 DB_PORT = 3306
-DB_USER = 'pixiv'
-DB_PASSWD = 'Huawei12#$'
+DB_USER = 'root'
+DB_PASSWD = 'root'
 DB_DATABASE = 'moe'
-# 数据库/表的编码设置为utf8mb4  
-# 插画信息中含emoji表情
 DB_CHARSET = "utf8mb4"
-# ================================================
+# ================== DataBase ==================
+
 
 # ===============DEBUG===============
 DEBUG = False
